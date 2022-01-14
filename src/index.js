37fc4162da939d701124c70ce666d649858bd2ce
Julia-itcourse <juliaartemenko93@gmail.com>
2022-01-14 16:27:16 +0100

    init: provide solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+  const absoluteNumber = Math.abs(n);
+  const reversedArr = Array.from(absoluteNumber.toString(), Number).reverse();
+  console.log("revArr", reversedArr);
+  const result = Number(reversedArr.join(""));
+  return result;
+};
